0e0dc7e78e57192922d30affe88c3b965c5d2c21
brunomaso1 <brunomaso1@hotmail.com>
2025-06-14 11:38:53 -0300

    feat: :construction_worker: SwinV2: en progreso

--- vision_transformer/utils.py
+++ vision_transformer/utils.py
@@ -1,6 +1,11 @@
 # Enum para los formatos de dataset
 from enum import Enum
 
+import datasets
+import evaluate
+from evaluate import Metric
+from sklearn.metrics import accuracy_score
+
 
 class DatasetFormat(Enum):
     """
@@ -36,3 +41,35 @@ class DatasetFormat(Enum):
             if fmt.value.lower() == format_str:
                 return fmt
         raise ValueError(f"Formato '{format_str}' no válido. Formatos disponibles: {cls.list_formats()}")
+
+
+class MulticlassAccuracy(Metric):
+    """Workaround for the default Accuracy class which doesn't support passing 'average' to the compute method.
+
+    Reference: https://stackoverflow.com/questions/76441777/huggingface-evaluate-function-use-multiple-labels
+    """
+
+    def _info(self):
+        return evaluate.MetricInfo(
+            description="Accuracy",
+            citation="",
+            inputs_description="",
+            features=datasets.Features(
+                {
+                    "predictions": datasets.Sequence(datasets.Value("int32")),
+                    "references": datasets.Sequence(datasets.Value("int32")),
+                }
+                if self.config_name == "multilabel"
+                else {
+                    "predictions": datasets.Value("int32"),
+                    "references": datasets.Value("int32"),
+                }
+            ),
+            reference_urls=["https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html"],
+        )
+
+    def _compute(self, predictions, references, normalize=True, sample_weight=None, **kwargs):
+        # take **kwargs to avoid breaking when the metric is used with a compute method that takes additional arguments
+        return {
+            "accuracy": float(accuracy_score(references, predictions, normalize=normalize, sample_weight=sample_weight))
+        }
